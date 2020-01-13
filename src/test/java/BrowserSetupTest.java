03aeb1b2a2065ca37be25196c00987e35f404ebb
Nata <natamoskvina@mail.ru>
2020-01-13 21:05:38 +0000

    Add influxdb support

--- src/test/java/BrowserSetupTest.java
+++ src/test/java/BrowserSetupTest.java
@@ -1,45 +1,30 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-@RunWith(Parameterized.class)
 public class BrowserSetupTest {
 
     private WebDriver driver;
-    private Browser browser;
     static final Logger logger = LogManager.getLogger(BrowserSetupTest.class.getName());
 
-    public BrowserSetupTest(Browser browser) {
-        this.browser = browser;
-    }
-
-    @Before
-    public void setupTest() {
-        BrowserDriverFactory factory = new BrowserDriverFactory(browser);
+    @BeforeTest
+    @Parameters({ "browser" })
+    public void setupTest(@Optional("chrome") String browser) {
+        BrowserDriverFactory factory = new BrowserDriverFactory(Browser.valueOf(browser.toUpperCase()));
         driver = factory.createDriver();
     }
 
-    @After
+    @AfterTest
     public void teardown() {
         if (driver != null) {
             driver.quit();
         }
-    }
-
-    @Parameterized.Parameters(name = "Browser: {0}")
-    public static Object[] browsers() {
-        return new Object[] { Browser.CHROME, Browser.FIREFOX };
     }
 
     @Test
@@ -47,7 +32,9 @@ public class BrowserSetupTest {
         logger.trace("Start test");
         String url = "https://otus.ru/";
         driver.get(url);
-        logger.error("This is error!");
-        logger.trace("End test");
+        logger.debug("Debug Message!");
+        logger.info("Info Message!");
+        logger.error("Error Message!");
+        logger.fatal("Fatal Message!");
     }
 }
